85881660f7b6507b994b7058fdf9d94f195c0297
David.seo <seojh5939@gmail.com>
2025-10-16 09:28:32 +0900

    기본 환경설정

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+local mapKey = require("utils.keyMapper").mapKey
+
+-- Neotree toggle
+mapKey('<leader>e', ':Neotree toggle<cr>')
+
+-- pane navigation
+mapKey('<C-h>', '<C-w>h') -- Left
+mapKey('<C-j>', '<C-w>j') -- Down
+mapKey('<C-k>', '<C-w>k') -- Up
+mapKey('<C-l>', '<C-w>l') -- Right
+
+-- clear search highlight
+mapKey('<leader>h', ':nohlsearch<CR>')
---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,7 @@
+return { "ellisonleao/gruvbox.nvim", 
+	priority = 1000 , 
+	lazy = false,
+	config = function()
+		vim.cmd([[colorscheme gruvbox]])
+	end, 
+}
